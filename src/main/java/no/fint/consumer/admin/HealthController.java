ddfd4441e7f862fe8925a71a6a8630ecb930a78e
jarlehansen <jarle@jarlehansen.net>
2017-05-26 20:59:50 +0200

    updated to version 0.0.15 of fint-event-model

--- src/main/java/no/fint/consumer/admin/HealthController.java
+++ src/main/java/no/fint/consumer/admin/HealthController.java
@@ -1,14 +1,10 @@
 package no.fint.consumer.admin;
 
-import com.google.common.collect.Lists;
 import no.fint.consumer.controller.Constants;
 import no.fint.consumer.event.Actions;
 import no.fint.consumer.event.ConsumerEventUtil;
 import no.fint.event.model.Event;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,15 +21,13 @@ public class HealthController {
 
     @RequestMapping
     public Event<String> healthCheck(@RequestHeader(value = Constants.HEADER_ORGID) String orgId,
-                                                    @RequestHeader(value = Constants.HEADER_CLIENT) String client) {
-
+                                     @RequestHeader(value = Constants.HEADER_CLIENT) String client) {
         Event<String> event = new Event<>(orgId, Constants.SOURCE, Actions.HEALTH.name(), client);
         Optional<Event> health = consumerEventUtil.healthCheck(event);
 
         if (health.isPresent()) {
-            return health.get();
-        }
-        else {
+            return new Event<>(health.get());
+        } else {
             event.setMessage("No response from adapter");
             return event;
         }
